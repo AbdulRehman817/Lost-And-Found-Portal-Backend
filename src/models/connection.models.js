03cad911584e2f03db3eb49219a16a236fd25a3b
Abdul Rehman <abdulrehmanbey1718@gmail.com>
2025-09-16 00:07:45 +0500

    timestamp added in connection model and default added at status

--- src/models/connection.models.js
+++ src/models/connection.models.js
@@ -1,19 +1,25 @@
 import mongoose from "mongoose";
-const connectionSchema = new mongoose.Schema({
-  requesterId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const connectionSchema = new mongoose.Schema(
+  {
+    requesterId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+      required: true,
+    },
   },
-  receiverId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  status: {
-    type: Boolean,
-    enum: ["pending", "accepted", "rejected"],
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 export const Connection = mongoose.model("Connection", connectionSchema);
