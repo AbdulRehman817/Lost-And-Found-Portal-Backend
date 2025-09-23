9d78f0e92c66847c18d3d1f991819e854fe8e963
Abdul Rehman <abdulrehmanbey1718@gmail.com>
2025-09-23 13:17:12 +0500

    chatModel created

---
+++ src/models/chat.models.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const chatSchema = new mongoose.Schema(
+  ({
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["sent", "delivered", "seen"],
+    },
+  },
+  { timestamps: true })
+);
+
+export const Chat = mongoose.model("Chat", chatSchema);
